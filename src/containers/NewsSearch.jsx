6e42b8f2aa6029251287c565a1a31954d4064cfb
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-12 16:21:44 -0700

    Initial app setup and test setup

---
+++ src/containers/NewsSearch.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './NewsSearch.css';
+
+export default class NewsSearch extends Component {
+	render() {
+		return (
+			<div className={style.newsSearch}>
+				<p>Loading...</p>
+			</div>
+		)
+	}
+}

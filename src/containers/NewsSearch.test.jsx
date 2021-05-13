a35aa02f2ab2ea7e18d85b3e608b4fdc53ea684d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-12 17:02:49 -0700

    Add textbox, button, and form to the test

--- src/containers/NewsSearch.test.jsx
+++ src/containers/NewsSearch.test.jsx
@@ -8,6 +8,9 @@ describe('News search component', () => {
   it('renders news search page', async () => {
     render(<NewsSearch />);
 
+	screen.getByRole('textbox', { name: 'search-input' });
+	screen.getByRole('button', { name: 'form-submit' });
+	screen.getByRole('form', { name: 'article-search' });
 	screen.getByText('Loading...');
 
 	const articleList = await screen.findByRole('list', { name: 'article-list' });
